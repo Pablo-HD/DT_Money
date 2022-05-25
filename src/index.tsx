9fadbc08077ce5c316e2fbf9624c792c89516cf9
Pablo H. Damini <phdamini@hotmail.com>
2022-05-24 21:05:45 -0300

    new route post mirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
+import { type } from 'os';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 600,
+          createdAt: new Date('2021-02-15 10:00:00'),
+
+        }
+
+      ]
+    })
+  },
+
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 01',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
     })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+
   }
 })
 
